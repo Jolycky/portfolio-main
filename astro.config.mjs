e7fa65c2f4647da0cf73fe3842507e4c592aa574
Jose Bagus <josebagus4@gmail.com>
2024-07-04 13:34:26 +0700

    chore: Update GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,23 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+
+
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://jolycky.github.io";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
+	server: { port: SERVER_PORT },
+  	site: BASE_URL,
 	integrations: [tailwind()],
 });
